c2254be2c896ad5ebccd045ff49513a1e11db284
jacob baumgartner <jacobbaumgartner@pcp077892pcs.wireless.calpoly.edu>
2014-02-08 14:31:32 -0800

    usercomments on icon

--- js/app.js
+++ js/app.js
@@ -3,14 +3,26 @@
 
 $(document).ready (function(){
 	$(".circle").draggable({helper: "clone"});
+	var counter = 0;
 	$(".dropzone").droppable({drop: function (e,ui) {
 		var element = $(ui.draggable).clone();
-		$(this).append(element);
+
+		var containerElement = $('.circlecontainer');
+		var iconType = 'exercise';
+		if(containerElement.hasClass('food')) {
+			iconType = 'food';
+		} 
+
+		var newelement = element.html('<div id="dropped'+counter+'" class="circlecontainer '+iconType+'"><div class="icon"></div><p class="activitycontext"></p></div>');
+		//$(this).append(element);
+		$(this).append(newelement);
 		var activity = prompt("Please enter your activity","Hiking");
 		if (activity!=null) {
 			x = "my activity: " +activity +".";
-			$(".activitycontext").html(x);
+			$('#dropped'+counter+' .activitycontext').html(x);
             dropRef.set(activity + ' time!');
 		}
+		
+		counter++;
 	}})
 })
