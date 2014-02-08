7380ae1424906903d101681c6eb06e75bf1d42c8
ElliotFiske <elliotfiske@gmail.com>
2014-02-08 14:10:12 -0800

    cool usernames

--- index.py
+++ index.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 
 import cgitb; cgitb.enable()
-import cgi, os
+import cgi, os, re
 
 # Required header that tells the browser how to render the text.
 print "Content-Type: text/html\n"
 
-# This tells us the URL that the user accessed
-print "REQUEST_URI: " + os.environ["REQUEST_URI"]
+usermatch = re.search(r'(?<=/)([A-z0-9\.]*?)$', os.environ['REQUEST_URI'])
+name = ''
+if usermatch is None:
+    name = "NOT LOGGED IN"
+else:
+    name = str(usermatch.group(1))
+    
+print 'USERNAME: ' + name
 
 html = open("index.html").read()
 
