92ae6f9f27093deceec1a94314ab8cc29e27da2e
ElliotFiske <elliotfiske@gmail.com>
2014-02-08 13:48:18 -0800

    cool new draggable icons

--- js/app.js
+++ js/app.js
@@ -1,16 +1,11 @@
 
 
-
 $(document).ready (function(){
-	$(".circle").draggable({helper: "clone"});
-	$(".dropzone").droppable({drop: function (e,ui) {
-		var element = $(ui.draggable).clone();
-		$(this).append(element);
-		var activity = prompt("Please enter your activity","Hiking");
-		if (activity!=null) {
-			x = "my activity: " +activity +".";
-			document.getElementById("activitycontext").innerHTML=x;
-            dropRef.set(activity + ' time!');
-		}
-	}})
+	$(".dragIcon").draggable({helper: "clone"});
+    
+    $('.droprow').droppable({drop: function(e, ui) {
+        var element = $(ui.draggable).clone();
+        $(this).empty();
+        $(this).append(element);
+    }});
 })
