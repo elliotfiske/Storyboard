57a70ce1d755b784480dcff9f512d9d8cb8f3b94
ElliotFiske <elliotfiske@gmail.com>
2014-02-08 17:48:57 -0800

    Added more icons = more fun

--- js/app.js
+++ js/app.js
@@ -24,6 +24,14 @@ $(document).ready (function(){
                 dropRef.update({activity4: activity});
                 break;
         }
+        
+        $("#activitydesc" + activityNum).html(desc);
+        $("#activityloc" + activityNum).html(loc);
+        
+        $("#activitylocation").val('');
+        $("#activitydescription").val('');
+        $("#activitytell").val('');
+        
 	});
     
 	$(".modal").hide(0);
@@ -36,20 +44,22 @@ $(document).ready (function(){
     });
 
 	$(".dragIcon").draggable({helper: "clone", 
-        cursorAt: {top: 0, left: 0, right: 0, bottom: 0}});
+        cursorAt: {top: 0, left: 0, right: 0, bottom: 0}
+    });
     
     var activityNum;
+    var newelement;
 	$(".droprow").droppable({drop: function (e,ui) {
 		var element = $(ui.draggable).clone();
 
  		activityNum = parseInt($(this).attr('id').match(/(\d+)$/)[0], 10);
         
-        var loc = $("#activitylocation").val();
-        var desc = $("#activitydescription").val();
+        var loc = '';
+        var desc = '';
 		var newelement = element.html('<div class="circlecontainer ' + 
         iconType + 
-        '"><div class="icon"></div><span class="activitydesc">' + desc +
-        '</span><span class="activityloc">' + loc + '</span></div>');
+        '"><div class="icon"></div><span id="activitydesc' + activityNum +
+        '"></span><br><span id="activityloc' + activityNum + '"></span></div>');
 
 		$(".modal").show(0);
 		$(".modaloverlay").show(0);
