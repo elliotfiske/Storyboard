88e6097890732caf9c23a269bdfd15e5300d4167
jacob baumgartner <jacobbaumgartner@pcp077892pcs.wireless.calpoly.edu>
2014-02-08 16:48:50 -0800

    formhide/unhide/hide

--- js/app.js
+++ js/app.js
@@ -1,35 +1,7 @@
 $(document).ready (function(){
-	$(".modal").addClass("ishidden");
-	$(".modaloverlay").addClass("ishidden");
-    var iconType = 'food';
-    
-    $('.circlecontainer').mousedown(function (e) {
-        iconType = $(this).attr('type');
-        console.log(iconType + 'clicked');
-    });
-
-	$(".dragIcon").draggable({helper: "clone"});
-    
-	$(".droprow").droppable({drop: function (e,ui) {
-		var element = $(ui.draggable).clone();
-
- 		var activityNum = parseInt($(this).attr('id').match(/(\d+)$/)[0], 10);
-        
-		var newelement = element.html('<div class="circlecontainer ' + iconType + '"><div class="icon"></div><span class="activitycontext"></span></div>');
-    
-		$(this).append(newelement);
-		$(".modal").removeClass("ishidden");
-		$(".modaloverlay").removeClass("ishidden");
-		// var activity = prompt("Please enter your activity that is " + iconType,"Hiking");
-		if (activity!=null) {
-			x = 'my activity: ' +activity +'.';
-			newelement.find('span').html(x);
-            dropRef.set(activity + ' time!');
-		}
-        
-        // Wipe the old children and add the new one
-        $(this).empty();
-        $(this).append(element);
+	$("#submit").click(function(e) {
+		$(".modal").hide(0);
+		$(".modaloverlay").hide(0);
 
         // Talk to firebase and tell it the new change
         switch (activityNum) {
@@ -46,6 +18,36 @@ $(document).ready (function(){
                 dropRef.update({activity4: activity});
                 break;
         }
+
+	});
+	$(".modal").hide(0);
+	$(".modaloverlay").hide(0);
+    var iconType = 'food';
+    
+    $('.circlecontainer').mousedown(function (e) {
+        iconType = $(this).attr('type');
+        console.log(iconType + 'clicked');
+    });
+
+	$(".dragIcon").draggable({helper: "clone"});
+    
+    var activityNum;
+	$(".droprow").droppable({drop: function (e,ui) {
+		var element = $(ui.draggable).clone();
+
+ 		activityNum = parseInt($(this).attr('id').match(/(\d+)$/)[0], 10);
+        
+		var newelement = element.html('<div class="circlecontainer ' + iconType + '"><div class="icon"></div><span class="activitycontext"></span></div>');
+    	
+		$(this).append(newelement);
+		$(".modal").show(0);
+		$(".modaloverlay").show(0);
+		// var activity = prompt("Please enter your activity that is " + iconType,"Hiking");
+
+        
+        // Wipe the old children and add the new one
+        $(this).empty();
+        $(this).append(element);
 	}});
 })
 
